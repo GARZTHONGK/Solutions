924942d8883879dcab5cf5f889234b8283287ba9
albe3955 <albert.bruun.thomsen@gmail.com>
2023-01-09 09:06:04 +0100

    final version

--- S0067_rpg2.py
+++ S0067_rpg2.py
@@ -32,26 +32,25 @@ class Character:
         self.attackpower = attackpower
         self.healpower = healpower
 
-
     def __repr__(self):
         return f" {self.name} {self.role} {self.max_health} {self._current_health} {self.attackpower} {self.healpower}"
-
 
     def melee(self, other):
         other._current_health -= self.attackpower
         print(self.name,"Used melee on", other.name,"!")
 
 
+
     # def get_hit(self, attackpower, other):
     #     self._current_health -= other.attackpower
 
-    def dead(self, character):
-        if self._current_health <= 0:
-            return True
+    def dead(self):
+        return self._current_health <= 0
+
 
 
     def heal(self, other):
-        if(self.role == "Healer"):
+        if(self.role == "Healer" or self.healpower > 0):
             other._current_health += self.healpower
             print(self.name, "Used Heal on", other.name,"!")
         else:
@@ -61,11 +60,14 @@ class Character:
 def printCharacters():
     print(character1)
     print(character2)
+    print(character3)
+    print(character4)
 
 
-
-character1 = Character("Torben", "Tank", 100, 20, 0) #  name, role, max_healt, attackpower, healpower
-character2 = Character("Rasmus", "Healer", 100, 5, 30)
+character1 = Character("Torben1", "Tank", 200, 10, 0) #  name, role, max_healt, attackpower, healpower
+character2 = Character("Rasmus2", "Healer", 100, 5, 30)
+character3 = Character("Frederik3", "Mage", 80, 20, 5)
+character4 = Character("Bob4", "Soldier", 120, 15, 5)
 # printCharacters()
 # character1.melee(character2)
 # printCharacters()
@@ -76,7 +78,30 @@ character2 = Character("Rasmus", "Healer", 100, 5, 30)
 # character2.melee(character1)
 turn = 0
 
-while not dead() and turn < 100:
+# 2v2, if a character dies the game ends
+while not character1.dead() and (not character2.dead()) and not character3.dead() and not character4.dead() and turn < 100:
     turn += 1
 
-printCharacters()
+    character1.melee(character3)
+    if character1._current_health <= 170:
+        character2.heal(character1)
+    else: character2.melee(character3)
+
+    if character3._current_health <= 10:
+        character3.heal(character3)
+    else:
+        character3.melee(character1)
+    if character4._current_health <= 10:
+        character4.heal(character4)
+    else:
+        character4.melee(character1)
+
+    if not character1.dead() or not  character2.dead() or not character3.dead() or not character4.dead():
+        print("\n A character has died! \n")
+
+    elif turn >= 100:
+        print("\n No characters have died! \n")
+        printCharacters()
+
+    print(printCharacters(),"turn:", turn)
+
